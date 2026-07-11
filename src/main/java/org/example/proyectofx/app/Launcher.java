030313602defa6093978edb0a60efbc122fd59ad
loorenna <lorenaalisson2004@gmail.com>
2026-07-11 17:46:56 -0500

    Actualización Conexion, agregación Launcher y edición de pom

---
+++ src/main/java/org/example/proyectofx/app/Launcher.java
@@ -0,0 +1,5 @@
+package org.example.proyectofx.app;
+import javafx.application.Application;
+public class Launcher {
+    public  static void main(String[] args){Application.launch(Main.class, args);}
+}
